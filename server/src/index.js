ddfeade40405b4fbfbd5d90f7676c0ca8c4a636c
Adriana <adri130306@gmail.com>
2026-04-08 17:45:28 +0200

    fix: vercel deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT || 3000;
-const taskRoutes = require('./routes/task.routes'); // Importamos las rutas
+const taskRoutes = require('./routes/task.routes');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors({
@@ -11,37 +11,43 @@ app.use(cors({
   methods: ['GET', 'POST', 'DELETE']
 }));
 app.use(express.json());
-app.use('/api/v1/tasks', taskRoutes);
-app.get('/health', (req, res) => res.json({ status: "ok" }));
-app.listen(PORT, () => console.log(`Servidor listo en puerto ${PORT}`));
 
-// Manejo Global de Excepciones 
+app.use('/api/v1/tasks', taskRoutes);
+
+// Ruta de prueba para verificar que el servidor vive
+app.get('/health', (req, res) => res.json({ status: "ok", message: "Servidor activo" }));
+
+//Para evitar el 404 inicial
+app.get('/', (req, res) => res.send('🚀 API de TaskFlow funcionando correctamente'));
+
+// Manejo Global de Errores
 app.use((err, req, res, next) => {
-    // 1. Mapeo semántico de errores
     if (err.message === 'NOT_FOUND') {
         return res.status(404).json({
             error: "Recurso no encontrado",
-            detail: "El ID solicitado no existe en nuestra base de datos."
+            detail: "El ID solicitado no existe."
         });
     }
 
     if (err.message === 'TITULO_REQUIRED') {
         return res.status(400).json({
             error: "Falta información",
-            detail: "El título de la tarea es obligatorio."
+            detail: "El título es obligatorio."
         });
     }
 
-    // 2. Registro de errores no controlados (Logs)
     console.error("[Server Error]:", err.stack);
-
-    // 3. Respuesta genérica para errores 500 (Seguridad)
     res.status(500).json({
         error: "Error interno del servidor",
-        message: "Algo salió mal de nuestro lado, estamos trabajando en ello."
+        message: "Algo salió mal en el backend."
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor TaskFlow en: http://localhost:${PORT}/api/v1/tasks`);
-});
+// Solo arranca el servidor con listen si NO estamos en Vercel
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`🚀 Servidor local en: http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
